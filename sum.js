494385576b714736c785e6e298219c241d9e5775
Francis Pellerin-Venne <francispellerinvenne@hotmail.fr>
2020-06-23 11:14:20 -0400

    whatever

---
+++ sum.js
@@ -0,0 +1,19 @@
+// - Write a node program that takes in an unlimited number of command line arguments, goes through each and prints out the sum of them. If any argument is not a whole number, skip it. Do support negative numbers though. If any argument is not a number, output an error message. We need at least 2 arguments.
+
+// takes in an unlimited number of command line arguments
+// We need at least 2 arguments.
+const argumentList = process.argv;
+const elements = argumentList.slice(2);
+
+
+// goes through each
+for(let i = 0; i < argumentList.length; i++) {
+  
+}
+// If any argument is not a whole number, skip it
+// Do support negative numbers though.
+// If any argument is not a number, output an error message.
+
+// add each number
+
+// print the sum
